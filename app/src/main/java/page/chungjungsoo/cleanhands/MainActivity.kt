5f44ba2fc5355bad980d94c766a98376f3301f12
Yejun Kim <yejun283@gmail.com>
2021-03-07 02:25:40 +0900

    notification setting

--- app/src/main/java/page/chungjungsoo/cleanhands/MainActivity.kt
+++ app/src/main/java/page/chungjungsoo/cleanhands/MainActivity.kt
@@ -1,5 +1,7 @@
 package page.chungjungsoo.cleanhands
 
+import android.content.Intent
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,6 +23,10 @@ class MainActivity : AppCompatActivity() {
 
         notiSettingBtn.setOnClickListener {
             // TODO: 노티 설정으로 이동 -> 환경절정 - 앱 - 우리앱에 노티설정
+            var intent = Intent()
+            intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+            intent.putExtra("android.provider.extra.APP_PACKAGE", packageName)
+            startActivity(intent)
         }
     }
 }
