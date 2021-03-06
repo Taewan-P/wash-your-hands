16b3b403121cd733625946333ad1c5e52beeb55e
Taewan Park <swimingtw@gmail.com>
2021-03-07 00:44:06 +0900

    Add buttons for features that we are going to implement

--- app/src/main/java/page/chungjungsoo/cleanhands/MainActivity.kt
+++ app/src/main/java/page/chungjungsoo/cleanhands/MainActivity.kt
@@ -2,10 +2,25 @@ package page.chungjungsoo.cleanhands
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setLocationBtn.setOnClickListener {
+            // TODO: 집 위치 설정하기
+        }
+
+        sensitivityBtn.setOnClickListener { 
+            // TODO: 감도 설정
+
+        }
+
+        notiSettingBtn.setOnClickListener {
+            // TODO: 노티 설정으로 이동 -> 환경절정 - 앱 - 우리앱에 노티설정
+        }
     }
 }
