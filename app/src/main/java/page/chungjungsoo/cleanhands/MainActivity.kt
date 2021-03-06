ea562ace568a6ef81d32fb82fe80a2a871f37ccb
Keunmo <9858momo@gmail.com>
2021-03-07 03:36:28 +0900

    add map

--- app/src/main/java/page/chungjungsoo/cleanhands/MainActivity.kt
+++ app/src/main/java/page/chungjungsoo/cleanhands/MainActivity.kt
@@ -2,10 +2,19 @@ package page.chungjungsoo.cleanhands
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.OnMapReadyCallback
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+//class MainActivity : AppCompatActivity(){
+
+    private lateinit var mMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,5 +31,17 @@ class MainActivity : AppCompatActivity() {
         notiSettingBtn.setOnClickListener {
             // TODO: 노티 설정으로 이동 -> 환경절정 - 앱 - 우리앱에 노티설정
         }
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val marker = LatLng(37.56, 126.97)
+        mMap.addMarker(MarkerOptions().position(marker).title("Home"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(16.0F))
     }
 }
