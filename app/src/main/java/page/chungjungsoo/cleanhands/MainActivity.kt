13a1c081ea8555b2d7a0aafe76280d37950e2d75
Taewan Park <swimingtw@gmail.com>
2021-03-07 18:22:37 +0900

    Add setHome feature

--- app/src/main/java/page/chungjungsoo/cleanhands/MainActivity.kt
+++ app/src/main/java/page/chungjungsoo/cleanhands/MainActivity.kt
@@ -1,25 +1,49 @@
 package page.chungjungsoo.cleanhands
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    private var myLocationRequest: LocationRequest? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        if (ContextCompat.checkSelfPermission(this@MainActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            } else {
+                ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            }
+        }
+
         var mapFragment: Fragment = MapFragment()
         supportFragmentManager.beginTransaction().replace(R.id.map, mapFragment, mapFragment.javaClass.simpleName).commit()
 
-        setLocationBtn.setOnClickListener {
-            // TODO: 집 위치 설정하기
-        }
 
         sensitivityBtn.setOnClickListener { 
             // TODO: 감도 설정
@@ -35,5 +59,32 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    if ((ContextCompat.checkSelfPermission(this@MainActivity,
+                            Manifest.permission.ACCESS_FINE_LOCATION) ==
+                                PackageManager.PERMISSION_GRANTED)) {
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    val alert = AlertDialog.Builder(this)
+                        .setTitle("Location Access Needed")
+                        .setPositiveButton("OK") { _, _ ->
+                            finish()
+                        }
+                        .create()
+                    alert.setCancelable(false)
+                    alert.show()
+                }
+                return
+            }
+        }
     }
 }
--- app/src/main/java/page/chungjungsoo/cleanhands/MapFragment.kt
+++ app/src/main/java/page/chungjungsoo/cleanhands/MapFragment.kt
@@ -1,11 +1,23 @@
 package page.chungjungsoo.cleanhands
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -17,12 +29,14 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.OnMapReadyCallback
 import kotlinx.android.synthetic.main.activity_map.*
 
-class MapFragment : Fragment(), OnMapReadyCallback {
+class MapFragment : Fragment(), OnMapReadyCallback, LocationListener {
     private lateinit var mMap: GoogleMap
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val defaultLocation = LatLng(37.57601, 126.97692) //Seoul, Gwanghwamoon
     private val locationPermissionGranted = false
     private val lastKnownLocation: Location? = null
+    lateinit var locationManager: LocationManager
+    private val locationPermissionCode = 2
 
     fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
         return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
@@ -42,6 +56,10 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         val innerMapFragment = innerMap as SupportMapFragment
         innerMapFragment.getMapAsync(this)
 
+        setLocationBtn.setOnClickListener {
+            getLocation()
+        }
+
 
     }
 
@@ -51,5 +69,33 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(marker).title("Home"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
         mMap.animateCamera(CameraUpdateFactory.zoomTo(16.0F))
+    }
+
+
+    private fun getLocation() {
+        locationManager = context!!.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(
+                this.context!!,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(
+                this.activity!!,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                locationPermissionCode
+            )
+        }
+        val location: Location? = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        Log.d("CURRENT LOCATION", "Lat: ${location!!.latitude}, Lon: ${location.longitude}")
+        mMap.clear()
+        val mark = LatLng(location.latitude, location.longitude)
+        mMap.addMarker((MarkerOptions().position(mark).title("Home")))
+        mMap.moveCamera((CameraUpdateFactory.newLatLng(mark)))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(16.0F))
+    }
+
+
+    // For when location is changed
+    override fun onLocationChanged(location: Location) {
+        Log.d("LOCATION", "Latitude: " + location.latitude + " , Longitude: " + location.longitude)
     }
 }
